60439ed9037689fb8e044575456171bf6d9b9eb3
Ren Yoshida <ren.yoshida1992@gmail.com>
2023-01-11 23:21:09 +0900

    戻るボタン改良

--- src/component/BackButton.tsx
+++ src/component/BackButton.tsx
@@ -1,13 +1,26 @@
 import {Button} from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import {Link} from "react-router-dom";
+import React, {useCallback, useEffect} from "react";
 
 /**
  * 戻るボタン
  */
 export const BackButton = () => {
+    // ctrl + ←で戻るボタンを動かす
+    const backAction = useCallback((event: any) => {
+        if (event.ctrlKey) {
+            if (event.code === "ArrowLeft") {
+                document.getElementById('back-button')?.click();
+            }
+        }
+    }, []);
+    useEffect(() => {
+        document.addEventListener("keydown", backAction, false);
+    }, []);
     return (
         <Button
+            id="back-button"
             variant="outlined"
             disableElevation
             sx={{
